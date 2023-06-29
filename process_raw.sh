00bb57a74dfe9da58ee7be0f4c7354efb240404e
kasra <kasrahajian2002@gmail.com>
2023-06-29 22:22:43 +0330

    Fix process_raw

--- process_raw.sh
+++ process_raw.sh
@@ -1,7 +1,7 @@
 raw_dir=data/raw
 for raw_filename in "$raw_dir"/*; do
   fits_filename="${raw_filename/.NEF/.FITS}"
-  fits_filename="${raw_filename/.CR2/.FITS}"
+  fits_filename="${fits_filename/.CR2/.FITS}"
   fits_filename="${fits_filename/raw/fits}"
   dcraw -c -w -v -4 -S 65535 $raw_filename | pnmtofits >$fits_filename #
 done
